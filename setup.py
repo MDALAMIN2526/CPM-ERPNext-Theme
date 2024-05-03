17b845b0c42e0072d106653b2d0657bd8dcba6c4
MDALAMIN2526 <amin252646@gmail.com>
2024-05-03 15:43:31 +0600

    Update

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
-# get version from __version__ variable in business_theme_v14/__init__.py
-from business_theme_v14 import __version__ as version
+# get version from __version__ variable in cmp_erpnext_theme/__init__.py
+from cmp_erpnext_theme import __version__ as version
 
 setup(
-	name="business_theme_v14",
+	name="cmp_erpnext_theme",
 	version=version,
 	description="Business Theme for ERPNext / Frappe",
 	author="Midocean Technologies Pvt Ltd",
